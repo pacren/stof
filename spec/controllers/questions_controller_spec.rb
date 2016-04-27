2c08c6fa59747641a2f891cc275a0a0a2f2d6ccc
pacren <knowmartian@gmail.com>
2016-04-27 15:04:21 +0400

    Create controller Question and index action

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'populates an array of questions' do
+      question1 = FactoryGirl.create(:question)
+      question2 = FactoryGirl.create(:question)
+
+      get :index
+
+      expect(assigns(:questions)).to match_array([question1, question2])
+    end
+
+    it 'render index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+end
